0aa7a8a2366dd0e554e072a1b93947824a8aa8ae
Patricio Frascaroli <pmfrascaroli@gmail.com>
2024-10-31 16:26:43 -0300

    Add function to backend.ts

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,9 +2,11 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource.js';
 import { data } from './data/resource.js';
 import { storage } from './storage/resource.js';
+import {transcribe} from "./functions/transcribe/resource";
 
 defineBackend({
   auth,
   data,
   storage,
+  transcribe,
 });
