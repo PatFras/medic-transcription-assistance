17390bb81f222812ea3cdeb215a285194ccf08f9
Patricio Frascaroli <pmfrascaroli@gmail.com>
2024-10-31 15:07:40 -0300

    login + upload

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,60 @@
 import { Authenticator } from '@aws-amplify/ui-react';
 import { Amplify } from 'aws-amplify';
-import awsconfig from '@/amplify_outputs.json'; // Asegúrate de que la ruta a tu archivo de configuración sea correcta
-import '@aws-amplify/ui-react/styles.css';
+import { uploadData } from 'aws-amplify/storage';
+import awsconfig from '@/amplify_outputs.json';
+import { useState, ChangeEvent } from 'react';
+import "@aws-amplify/ui-react/styles.css";
 
 Amplify.configure(awsconfig);
 
 const HomePage = () => {
+  const [file, setFile] = useState<File | null>(null);
+  const [uploadProgress, setUploadProgress] = useState<number | null>(null);
+  const [transcription, setTranscription] = useState<string>("");
+
+  const pickFile = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files) {
+      setFile(event.target.files[0]);
+    }
+  };
+
+  const uploadFile = async () => {
+    if (file) {
+      const fileReader = new FileReader();
+      fileReader.readAsArrayBuffer(file);
+      fileReader.onload = async (event: ProgressEvent<FileReader>) => {
+        if (event.target?.result) {
+          console.log("Complete File read successfully!", event.target.result);
+          try {
+            await uploadData({
+              data: event.target.result as ArrayBuffer,
+              path: file.name,
+              options: {
+                contentType: file.type
+              }
+            });
+            console.log('File uploaded successfully');
+          } catch (error) {
+            console.log("error", error);
+          }
+        } else {
+          console.log('Error: event.target.result is undefined');
+        }
+      };
+    }
+  };
+
   return (
     <Authenticator>
       {({ signOut, user }) => (
         <main>
           <h1>Hello {user?.username}</h1>
           <button onClick={signOut}>Sign out</button>
-          <p>Welcome to the homepage!</p>
+          <div>
+            <input title='file' type="file" onChange={pickFile} />
+            <button onClick={uploadFile}>Upload and Transcribe</button>
+          </div>
+          {uploadProgress !== null && <p>Upload Progress: {uploadProgress.toFixed(2)}%</p>}
         </main>
       )}
     </Authenticator>
