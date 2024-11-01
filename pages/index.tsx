cdb49e0e3c00ff6ca7684d0afa4bda7102703519
Patricio Frascaroli <pmfrascaroli@gmail.com>
2024-11-01 16:48:56 -0300

    update API REST uploadFile

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ Amplify.configure(awsconfig);
 const HomePage = () => {
   const [file, setFile] = useState<File | null>(null);
   const [uploadProgress, setUploadProgress] = useState<number | null>(null);
-  const [transcription, setTranscription] = useState<string>("");
+  const [transcription, setTranscription] = useState<any>(null); // Cambiar a `any` para manejar cualquier tipo de respuesta
 
   const pickFile = (event: ChangeEvent<HTMLInputElement>) => {
     if (event.target.files) {
@@ -20,44 +20,45 @@ const HomePage = () => {
 
   const uploadFile = async () => {
     if (file) {
-        const fileReader = new FileReader();
-        fileReader.readAsArrayBuffer(file);
-        fileReader.onload = async (event: ProgressEvent<FileReader>) => {
-            if (event.target?.result) {
-                console.log("Complete File read successfully!", event.target.result);
-                try {
-                    const result = await uploadData({
-                        data: event.target.result as ArrayBuffer,
-                        path: file.name,
-                        options: {
-                            contentType: file.type
-                        }
-                    });
-                    console.log('File uploaded successfully');
+      const fileReader = new FileReader();
+      fileReader.readAsArrayBuffer(file);
+      fileReader.onload = async (event: ProgressEvent<FileReader>) => {
+        if (event.target?.result) {
+          console.log("Complete File read successfully!", event.target.result);
+          try {
+            const result = await uploadData({
+              data: event.target.result as ArrayBuffer,
+              path: file.name,
+              options: {
+                contentType: file.type
+              }
+            });
+            console.log('File uploaded successfully');
 
-                    // Llama a la función Lambda para transcribir el audio
-                    const response = await fetch('https://fl4rn02ej0.execute-api.sa-east-1.amazonaws.com/dev/my-transcription', {
-                        method: 'POST',
-                        headers: {
-                            'Content-Type': 'application/json'
-                        },
-                        body: JSON.stringify({
-                            audioFileKey: file.name,
-                            languageCode: 'es-ES'
-                        })
-                    });
+            // Llama a la función Lambda para transcribir el audio
+            const response = await fetch('https://fl4rn02ej0.execute-api.sa-east-1.amazonaws.com/dev/transcribe', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json'
+              },
+              body: JSON.stringify({
+                audioFileKey: file.name,
+                languageCode: 'es-ES'
+              })
+            });
 
-                    const data = await response.json();
-                    setTranscription(data);
-                } catch (error) {
-                    console.log("error", error);
-                }
-            } else {
-                console.log('Error: event.target.result is undefined');
-            }
-        };
+            const data = await response.json();
+            const parsedData = JSON.parse(data); // Parsea la respuesta JSON
+            setTranscription(parsedData);
+          } catch (error) {
+            console.log("error", error);
+          }
+        } else {
+          console.log('Error: event.target.result is undefined');
+        }
+      };
     }
-};
+  };
 
   return (
     <Authenticator>
@@ -66,14 +67,14 @@ const HomePage = () => {
           <h1>Hello {user?.username}</h1>
           <button onClick={signOut}>Sign out</button>
           <div>
-            <input title='file' type="file" onChange={pickFile} />
+            <input title="file" type="file" onChange={pickFile} />
             <button onClick={uploadFile}>Upload and Transcribe</button>
           </div>
           {uploadProgress !== null && <p>Upload Progress: {uploadProgress.toFixed(2)}%</p>}
           {transcription && (
             <div>
               <h2>Transcription</h2>
-              <p>{transcription}</p>
+              <pre>{transcription.results.transcripts[0].transcript}</pre>
             </div>
           )}
         </main>
